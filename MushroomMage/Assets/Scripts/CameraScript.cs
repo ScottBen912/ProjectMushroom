7fc4bfc37d3ee3475aa3313810c6c9cc8f9c731f
Ben Scott <benfox@artlover.com>
2024-07-29 22:49:32 -0500

    Project Remade

--- MushroomMage/Assets/Scripts/CameraScript.cs
+++ MushroomMage/Assets/Scripts/CameraScript.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject character;
+    public Vector3 startposition;
     float xmove;
     float ymove;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startposition = transform.position;
     }
 
     // Update is called once per frame
@@ -21,6 +22,6 @@ public class CameraScript : MonoBehaviour
         ymove += Input.GetAxis("Mouse X") * Time.deltaTime * 5;
         xmove = Mathf.Clamp(xmove, -1f, 1f);
         ymove = Mathf.Clamp(ymove, -1f, 1f);
-        transform.position = new Vector3(character.transform.position.x + ymove, character.transform.position.y + xmove, -10f);
+        transform.position = new Vector3((character.transform.position.x + startposition.x) + ymove,startposition.y, (character.transform.position.z + startposition.z) + xmove);
     }
 }
--- MushroomMage/Assets/Scripts/CharacterScript.cs
+++ MushroomMage/Assets/Scripts/CharacterScript.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.EventSystems;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterScript : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    public Animator anim;
     public GameObject reticle;
-    public Rigidbody2D rb;
+    public Rigidbody rb;
+    public Vector3 direction;
+    public Vector3 mouseWorldPosition;
+    public float angle;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +21,26 @@ public class CharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPosition.z = 0f;
+        Ray ray1 = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray1, out RaycastHit raycastHit1))
+        {
+            mouseWorldPosition = new Vector3(raycastHit1.point.x, 0, raycastHit1.point.z);
+        }
         reticle.transform.position = mouseWorldPosition;
+        direction = reticle.transform.position - transform.position;
+        transform.LookAt(mouseWorldPosition);
+        mainCamera.ScreenToWorldPoint(Input.mousePosition);
         float dirX = Input.GetAxisRaw("Horizontal");
-        float dirY = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector2(dirX * 8f, dirY * 8f);
+        float dirZ = Input.GetAxisRaw("Vertical");
+        Debug.Log(transform.position);
+        if (dirX != 0 || dirZ != 0)
+        {
+            anim.SetBool("IsMoving", true);
+        }
+        else if (dirX == 0 && dirZ == 0)
+        {
+            anim.SetBool("IsMoving", false);
+        }
+        rb.velocity = new Vector3(dirX * 12f, 0f,dirZ * 12f);
     }
 }

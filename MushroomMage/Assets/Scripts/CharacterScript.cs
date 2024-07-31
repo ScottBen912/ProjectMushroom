a1785aa7741d56281c5f9facf1424c49b17af6f0
jakeheck7 <W213650069@student.hccs.edu>
2024-07-31 01:31:48 -0500

    Enemy health (balls collide with each other to create velocity?)

--- MushroomMage/Assets/Scripts/CharacterScript.cs
+++ MushroomMage/Assets/Scripts/CharacterScript.cs
@@ -19,7 +19,7 @@ public class CharacterScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Ray ray1 = mainCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray1, out RaycastHit raycastHit1))
--- MushroomMage/Assets/Scripts/EnemyBehaviorAndHealth1.cs
+++ MushroomMage/Assets/Scripts/EnemyBehaviorAndHealth1.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyBehaviorAndHealth1 : MonoBehaviour
 {
+
+    public int maxHealth = 3;
+    public int currentHealth;
+    public int amount = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnCollisionEnter (Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Bullit")
+        {
+            currentHealth -= amount;
+            if (currentHealth < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }

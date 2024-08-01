0a787001236bc4f7e0c8551175a62ea9251edba7
jakeheck7 <W213650069@student.hccs.edu>
2024-08-01 15:03:44 -0500

    Added SceneManage Additive
    In the Hierarchy you can choose to work in James's folder which is where the art should go. You can also make the scene invisible or visible. The gameLoad (or, when you press play) still loads content in James's scene whether you've made it invisible or not.

---
+++ MushroomMage/Assets/Scripts/SceneManage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+            SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+}

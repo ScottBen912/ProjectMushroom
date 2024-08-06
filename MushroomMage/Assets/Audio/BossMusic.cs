0974bfccd54b79f7cbb09a6fc1c2b5e9375322d4
JonMark2024 <jonsustaita@hotmail.com>
2024-08-05 20:17:03 -0500

    Created a script for the Enemy Placeholder...
    Created a script for the Enemy Placeholder as component, added some code there and readded audio source component.

--- MushroomMage/Assets/Audio/BossMusic.cs
+++ MushroomMage/Assets/Audio/BossMusic.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BossMusic : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip BossMusic, defaultMusic; 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame

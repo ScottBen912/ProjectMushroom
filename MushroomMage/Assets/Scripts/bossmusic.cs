f84af301af163ec57c3469293e2904b5b056f04f
jakeheck7 <W213650069@student.hccs.edu>
2024-08-07 19:17:34 -0500

    Audio
    Fixed footstep sounds ("//" script referencing sprinting since we don't have that mechanic or audio and it was giving errors), made a trigger system that mutes the background music and plays the boss music

--- MushroomMage/Assets/Scripts/bossmusic.cs
+++ MushroomMage/Assets/Scripts/bossmusic.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class bossmusic : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioSource audioBackground;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Wizard" && !audioSource.isPlaying)
+        if (other.tag == "Player")
         {
+            audioSource.mute = false;
             audioSource.Play();
+            audioBackground.mute = true;
         }
     }
 }
